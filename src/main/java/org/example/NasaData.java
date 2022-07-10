94051dd109bdc4f387587654c7886c00700289a3
artsmmy <106704118+artsmmy@users.noreply.github.com>
2022-07-10 19:13:41 +0300

    Update NasaData.java

--- src/main/java/org/example/NasaData.java
+++ src/main/java/org/example/NasaData.java
@@ -7,8 +7,8 @@ public class NasaData {
     private String date;
     private String explanation;
     private String hdurl;
-    private String media_type;
-    private String service_version;
+    private String mediaType;
+    private String serviceVersion;
     private String title;
     private String url;
 
@@ -17,8 +17,8 @@ public class NasaData {
             @JsonProperty("date") String date,
             @JsonProperty("explanation") String explanation,
             @JsonProperty("hdurl") String hdurl,
-            @JsonProperty("media_type") String media_type,
-            @JsonProperty("service_version") String service_version,
+            @JsonProperty("media_type") String mediaType,
+            @JsonProperty("service_version") String serviceVersion,
             @JsonProperty("title") String title,
             @JsonProperty("url") String url
             ) {
@@ -26,8 +26,8 @@ public class NasaData {
                 this.date = date;
                 this.explanation = explanation;
                 this.hdurl = hdurl;
-                this.media_type = media_type;
-                this.service_version = service_version;
+                this.mediaType = mediaType;
+                this.serviceVersion = serviceVersion;
                 this.title = title;
                 this.url = url;
     }
@@ -49,11 +49,11 @@ public class NasaData {
     }
 
     public String getMedia_type() {
-        return media_type;
+        return mediaType;
     }
 
     public String getService_version() {
-        return service_version;
+        return serviceVersion;
     }
 
     public String getTitle() {
@@ -71,8 +71,8 @@ public class NasaData {
                 ", date='" + date + '\'' +
                 ", explanation='" + explanation + '\'' +
                 ", hdurl='" + hdurl + '\'' +
-                ", media_type='" + media_type + '\'' +
-                ", service_version='" + service_version + '\'' +
+                ", media_type='" + mediaType + '\'' +
+                ", service_version='" + serviceVersion + '\'' +
                 ", title='" + title + '\'' +
                 ", url='" + url + '\'' +
                 '}';
